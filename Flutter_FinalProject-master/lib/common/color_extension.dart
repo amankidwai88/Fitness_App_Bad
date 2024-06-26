004919452e782d5ee3cc810e56fadfcf8f64c733
amankidwai88 <amanrazi870@gmail.com>
2024-06-26 15:12:19 +0500

    no new change

--- Flutter_FinalProject-master/lib/common/color_extension.dart
+++ Flutter_FinalProject-master/lib/common/color_extension.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TColor {
-  static Color get primary => const Color(0xff9DCEFF);
+  static Color get primary => const Color(0xffFF9B70);
 
   static Color get primaryText => const Color(0xff221E3A);
   static Color get secondaryText => const Color(0xff707070);
@@ -16,9 +16,6 @@ class TColor {
 
   static Color get secondaryColor1 => const Color(0xffC58BF2);
   static Color get secondaryColor2 => const Color(0xffEEA4CE);
-
-  // static Color? get backgrouncolor => Colors.grey[300];
-  static Color? get backgrouncolor => Colors.white;
 
   static List<Color> get primaryG => [primaryColor2, primaryColor1];
   static List<Color> get secondaryG => [secondaryColor2, secondaryColor1];
